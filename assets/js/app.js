15e441ef13c405208088cec4444bfdfcdf2f4a48
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-02-18 18:51:56 +0000

    Initial commit and basic list view

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var http = new XMLHttpRequest();
+http.open("GET", "http://api.devnews.today", false);
+http.send(null);
+
+json = JSON.parse('{"items":' + http.responseText + '}');
+
+var source   = document.getElementById('news-template').innerHTML;
+var template = Handlebars.compile(source);
+
+document.getElementById('main').innerHTML = template(json);
