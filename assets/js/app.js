bc98dc348ea68a0d9ce86d6647e7c436271818ec
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-02-21 00:09:20 +0000

    Supports aync API calling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,23 @@
+var nojs = document.querySelector(".nojs")
+nojs.parentNode.removeChild(nojs);
+
+document.getElementById('main').innerHTML += "<p class='loading'>Grabbing fresh data from <strong>HackerNews</strong> and <strong>Reddit</strong>...</p>";
+
 var http = new XMLHttpRequest();
-http.open("GET", "http://api.devnews.today", false);
+http.open("GET", "http://api.devnews.today", true);
+
+http.onload = function () {
+	if (http.readyState === 4) {
+		json = JSON.parse('{"items":' + this.responseText + '}');
+
+		var source   = document.getElementById('news-template').innerHTML;
+		var template = Handlebars.compile(source);
+
+		var loading = document.querySelector(".loading")
+		loading.parentNode.removeChild(loading);
+
+		document.getElementById('main').innerHTML += template(json);
+	}
+};
+
 http.send(null);
-
-json = JSON.parse('{"items":' + http.responseText + '}');
-
-var source   = document.getElementById('news-template').innerHTML;
-var template = Handlebars.compile(source);
-
-document.getElementById('main').innerHTML += template(json);
