d4e88fe163f23f70ea67ed0e481e5684961782c5
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-05-13 20:47:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ var server = app.listen(3000, '0.0.0.0', function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('devnews-web http://%s:%s', host, port);
 
 });
